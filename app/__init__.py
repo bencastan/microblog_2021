d978e901e5f60e112e93e15dfde94a1fa6f022c5
Ben Castan <ben@castanfamily.com>
2021-06-03 11:05:00 +1000

    End chapter 14

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,7 @@
-
-import os
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 from flask import Flask, request
-from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
@@ -11,7 +9,7 @@ from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_babel import Babel, lazy_gettext as _l
-
+from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -19,14 +17,12 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
-login.login_message = (_('Please log in to access this page.'))
+login.login_message = _l('Please log in to access this page.')
 mail = Mail(app)
-bootstrap =  Bootstrap(app)
+bootstrap = Bootstrap(app)
 moment = Moment(app)
 babel = Babel(app)
 
-
-# Email logging Handler
 if not app.debug:
     if app.config['MAIL_SERVER']:
         auth = None
@@ -42,7 +38,6 @@ if not app.debug:
             credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
-        # Logging to file
 
     if not os.path.exists('logs'):
         os.mkdir('logs')
@@ -52,6 +47,7 @@ if not app.debug:
         '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
     file_handler.setLevel(logging.INFO)
     app.logger.addHandler(file_handler)
+
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
 
